d76e59c240ea81102f03d57e4bf9a375cd589474
Luna Smith <artistlunasmith@gmail.com>
2023-05-01 19:54:31 +0100

    Updated CreatePage

--- vue-training/src/components/CreatePage.vue
+++ vue-training/src/components/CreatePage.vue
@@ -3,14 +3,24 @@
     <form action="">
       <div mb-3>
         <label for="" class="form-label"> Page Title </label>
-        <input type="text" class="form-control" />
+        <input
+          type="text"
+          class="form-control"
+          :value="pageTitle"
+          @input="(e) => (pageTitle = e.target.value)"
+        />
       </div>
       <div mb-3>
         <label for="" class="form-label"> Content </label>
         <textarea type="text" class="form-control" rows="5"></textarea>
       </div>
       <div mb-3>
-        <button class="btn btn-primary">Create Page</button>
+        <button
+          class="btn btn-primary"
+          @click.prevent="pageCreated({ pageTitle })"
+        >
+          Create Page
+        </button>
       </div>
     </form>
   </div>
@@ -19,5 +29,10 @@
 <script>
 export default {
   props: ["pageCreated"],
+  data() {
+    return {
+      pageTitle: "",
+    };
+  },
 };
 </script>
