48590730f870071f2919bdb87003e0d87779b0bd
Luna Smith <artistlunasmith@gmail.com>
2023-05-02 12:54:52 +0100

    Updated CreatePage

--- vue-training/src/components/CreatePage.vue
+++ vue-training/src/components/CreatePage.vue
@@ -1,26 +1,44 @@
 <template>
   <div class="container mb-3">
     <form action="">
-      <div mb-3>
-        <label for="" class="form-label"> Page Title </label>
-        <input type="text" class="form-control" v-model="pageTitle" />
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Page Title </label>
+          <input type="text" class="form-control" v-model="pageTitle" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Content </label>
+          <textarea
+            type="text"
+            class="form-control"
+            rows="5"
+            v-model="content"
+          ></textarea>
+        </div>
+        <div class="mb-3">
+          <button
+            class="btn btn-primary"
+            @click.prevent="pageCreated({ pageTitle, content })"
+          >
+            Create Page
+          </button>
+        </div>
       </div>
-      <div mb-3>
-        <label for="" class="form-label"> Content </label>
-        <textarea
-          type="text"
-          class="form-control"
-          rows="5"
-          v-model="content"
-        ></textarea>
-      </div>
-      <div mb-3>
-        <button
-          class="btn btn-primary"
-          @click.prevent="pageCreated({ pageTitle, content })"
-        >
-          Create Page
-        </button>
+      <div class="col">
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Text </label>
+          <input type="text" class="form-control" />
+        </div>
+        <div class="mb-3">
+          <label for="" class="form-label"> Link Url </label>
+          <input type="text" class="form-control" />
+        </div>
+        <div class="row mb-3">
+          <div class="form-check">
+            <input class="form-check-input" type="checkbox" />
+            <label class="form-check-label" for="checkbox">Published</label>
+          </div>
+        </div>
       </div>
     </form>
   </div>
