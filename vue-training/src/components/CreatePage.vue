139da3d0b884c3639decba3ac75a74802b164864
Luna Smith <artistlunasmith@gmail.com>
2023-05-02 13:36:58 +0100

    Updated CreatePage.vue

--- vue-training/src/components/CreatePage.vue
+++ vue-training/src/components/CreatePage.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container mb-3">
-    <form action="">
-      <div class="col">
+  <form action="" class="container mb-3">
+    <div class="row">
+      <div class="col-md-8">
         <div class="mb-3">
           <label for="" class="form-label"> Page Title </label>
           <input type="text" class="form-control" v-model="pageTitle" />
@@ -15,33 +15,30 @@
             v-model="content"
           ></textarea>
         </div>
-        <div class="mb-3">
-          <button
-            class="btn btn-primary"
-            @click.prevent="pageCreated({ pageTitle, content })"
-          >
-            Create Page
-          </button>
-        </div>
       </div>
       <div class="col">
         <div class="mb-3">
           <label for="" class="form-label"> Link Text </label>
-          <input type="text" class="form-control" />
+          <input type="text" class="form-control" v-model="linkText" />
         </div>
         <div class="mb-3">
           <label for="" class="form-label"> Link Url </label>
-          <input type="text" class="form-control" />
+          <input type="text" class="form-control" v-model="linkUrl" />
         </div>
         <div class="row mb-3">
           <div class="form-check">
             <input class="form-check-input" type="checkbox" />
-            <label class="form-check-label" for="checkbox">Published</label>
+            <label class="form-check-label" for="gridCkeck1">Published</label>
           </div>
         </div>
       </div>
-    </form>
-  </div>
+    </div>
+    <div class="mb-3">
+      <button class="btn btn-primary" @click.prevent="submitForm">
+        Create Page
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -51,7 +48,23 @@ export default {
     return {
       pageTitle: "",
       content: "",
+      linkText: "",
+      linkUrl: "",
     };
+  },
+  methods: {
+    submitForm() {
+      if (!this.pageTitle || !this.content || !this.linkText || !this.linkUrl) {
+        alert("Please fill the form.");
+        return;
+      }
+      this.pageCreated({
+        pageTitle: this.pageTitle,
+        content: this.content,
+        linktText: this.linkText,
+        linkUrl: this.linkUrl,
+      });
+    },
   },
 };
 </script>
